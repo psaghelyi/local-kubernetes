5d8d075b18b21e55e90a81bfdf7edf700dab2716
Saghelyi, Peter <psaghelyi@gmail.com>
2022-04-17 10:31:33 +0200

    placeholders added

---
+++ 3-k0s/create-cluster.sh
@@ -0,0 +1,2 @@
+
+docker run -d --name k0s --hostname k0s --privileged -v /var/lib/k0s -p 6443:6443 docker.io/k0sproject/k0s:latest
---
+++ 6-kind/create-cluster.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+
+cat > kind-config.yaml <<EOF
+# three node (two workers) cluster config
+kind: Cluster
+apiVersion: kind.x-k8s.io/v1alpha4
+nodes:
+- role: control-plane
+- role: worker
+- role: worker
+EOF
+
+kind create cluster --name k8s-playground --config kind-config.yaml
+
+rm kind-config.yaml
